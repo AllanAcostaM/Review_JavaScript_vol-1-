3acd5f1d4be9bff192aaf4a6766b5b694c753cd3
Jossep Acosta <allanacost.27@gmail.com>
2025-08-06 23:16:51 -0500

    tipos - comentarios

---
+++ 01-tipos/05-comentarios.js
@@ -0,0 +1,15 @@
+let numero = 42; // Este es otro comentario
+
+/**
+ * Este es un comentario 
+ * de muchas
+ * lineas
+ */
+
+/*
+Todo lo que escribamos entremedio 
+de los slash y los asterisco 
+va a ser ignorado
+*/
+
+console.log(numero);

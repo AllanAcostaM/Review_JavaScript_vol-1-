1fc659ca6a49e720121f27cef3c745d9a66c3c0c
Jossep Acosta <allanacost.27@gmail.com>
2025-08-06 22:54:02 -0500

    tipos - primitivos

---
+++ 01-tipos/02-primitivos.js
@@ -0,0 +1,8 @@
+let numero = 1; // Number
+let texto = "Hola mundo"; // String
+let verdadero = true; // Boolean (true)
+let falso = false; // Boolean (false)
+let noDefinido; // Undefined
+let undef = undefined; // Undefined 
+let nulo = null; // null
+

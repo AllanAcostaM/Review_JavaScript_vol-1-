3c078eee8317dee1c37bef46c54d0e881e8b1770
Jossep Acosta <allanacost.27@gmail.com>
2025-08-06 22:48:10 -0500

    variables y tipos de datos

---
+++ 01-tipos/01-variables.js
@@ -0,0 +1,12 @@
+let nombre = "Hola Mundo";
+let NombreCompleto; // UpperCamelCase
+let nombreCompleto; //camelCase
+let nombre_completo; //snake_case
+
+console.log(nombre);
+
+let apellido;
+let animal;
+apellido = "Acosta";
+
+

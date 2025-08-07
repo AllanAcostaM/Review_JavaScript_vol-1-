01b1b0c69856ee3215332d40fdd8e17414740f32
Jossep Acosta <allanacost.27@gmail.com>
2025-08-06 23:01:06 -0500

    tipos - constantes

---
+++ 01-tipos/03-constantes.js
@@ -0,0 +1,9 @@
+const nombre = "Hola Mundo";
+nombre = "Chanchito feliz"
+
+console.log(nombre);
+
+/* Nota de recordatorio: Los valores de las variables generalmente no debrian ser
+                         alterados, sino que cada variable deberia de contar con su propio valor
+                         tal como en el ejemplo de aplicar "constantes" (const) en vez de "variables" (let o var)*/
+                         
